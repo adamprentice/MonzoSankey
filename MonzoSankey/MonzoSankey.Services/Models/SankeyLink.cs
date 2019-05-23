e0f7d6d6e0fa7c3c35d05c15670d51e438a2ba30
Adam Prentice <adamjlprentice@gmail.com>
2019-05-24 00:39:05 +0100

    Getting an actual sankey diagram showing!

---
+++ MonzoSankey/MonzoSankey.Services/Models/SankeyLink.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonzoSankey.Services.Models
+{
+    public class SankeyLink
+    {
+        public SankeyLink(int source, int target, double value)
+        {
+            this.Source = source;
+            this.Target = target;
+            this.Value = value;
+        } 
+
+        public int Source { get; set; } // index of source in Nodes
+
+        public int Target { get; set; } // index of target in Nodes
+
+        public double Value { get; set; }
+
+        public string Color { get; set; }
+
+        public string Key { get; set; }
+
+        public int? Opacity { get; set; }
+    }
+}
--- MonzoSankey/MonzoSankey.Services/Transactions/ITransactionService.cs
+++ MonzoSankey/MonzoSankey.Services/Transactions/ITransactionService.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using MonzoApi;
 using MonzoSankey.Core.Models;
+using MonzoSankey.Services.Models;
 
 namespace MonzoSankey.Services.Transactions
 {
     public interface ITransactionService
     {
-        IEnumerable<Transaction> GetTransactions(MonzoClient client, DateTime? from = null, DateTime? to = null);
+        SankeyResponse GetSankeyDataForTransactions(MonzoClient client, DateTime? from = null, DateTime? to = null);
     }
 }
